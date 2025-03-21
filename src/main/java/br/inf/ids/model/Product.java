45fa84992718322b00bf42a013d3e4859ae89cf9
João Pedor Alves <joaopedroalvesgv@gmail.com>
2025-03-21 15:48:04 -0300

    Criada pasta model e subpasta enums. Criado Product model e ProdcutStatus enum

---
+++ src/main/java/br/inf/ids/model/Product.java
@@ -0,0 +1,54 @@
+package br.inf.ids.model;
+
+import br.inf.ids.model.enums.ProductStatus;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String productId;
+
+    private String description;
+
+    @Column(nullable = false)
+    private ProductStatus productStatus;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ProductStatus getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(ProductStatus productStatus) {
+        this.productStatus = productStatus;
+    }
+}
---
+++ src/main/java/br/inf/ids/model/enums/ProductStatus.java
@@ -0,0 +1,6 @@
+package br.inf.ids.model.enums;
+
+public enum ProductStatus {
+    ACTIVE,
+    INACTIVE
+}
