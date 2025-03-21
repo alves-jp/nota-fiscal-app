dd30792446190493a858a57957e387e144716162
João Pedor Alves <joaopedroalvesgv@gmail.com>
2025-03-21 16:55:13 -0300

    Criado pastas Service e ServiceImpl. ProductService e ProductServiceImpl criados

---
+++ src/main/java/br/inf/ids/service/ProductService.java
@@ -0,0 +1,21 @@
+package br.inf.ids.service;
+
+import br.inf.ids.model.Product;
+import br.inf.ids.model.enums.ProductStatus;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+
+    Product createProduct(Product product);
+
+    Optional<Product> findProductById(Long id);
+
+    List<Product> findAllProducts();
+
+    List<Product> findProductsByStatus(ProductStatus status);
+
+    Product updateProduct(Long id, Product product);
+
+    void deleteProduct(Long id);
+}
---
+++ src/main/java/br/inf/ids/service/impl/ProductServiceImpl.java
@@ -0,0 +1,64 @@
+package br.inf.ids.service.impl;
+
+import br.inf.ids.model.Product;
+import br.inf.ids.model.enums.ProductStatus;
+import br.inf.ids.repository.ProductRepository;
+import br.inf.ids.service.ProductService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class ProductServiceImpl implements ProductService {
+
+    @Inject
+    ProductRepository productRepository;
+
+    @Override
+    @Transactional
+    public Product createProduct(Product product) {
+        productRepository.persist(product);
+
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findProductById(Long id) {
+        return productRepository.findByIdOptional(id);
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        return productRepository.listAll();
+    }
+
+    @Override
+    public List<Product> findProductsByStatus(ProductStatus status) {
+        return productRepository.findByProductStatus(status);
+    }
+
+    @Override
+    @Transactional
+    public Product updateProduct(Long id, Product product) {
+        Product existingProduct = productRepository.findById(id);
+
+        if (existingProduct != null) {
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setProductStatus(product.getProductStatus());
+        }
+
+        return existingProduct;
+    }
+
+    @Override
+    @Transactional
+    public void deleteProduct(Long id) {
+        Product product = productRepository.findById(id);
+
+        if (product != null) {
+            productRepository.delete(product);
+        }
+    }
+}
